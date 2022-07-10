b4033cc1187e1e5e966838d6c66a60387660804d
Luis Espinal <luisespinal294@gmail.com>
2022-07-10 14:23:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function twoSmallest(banana) {
-    let smallestNumber = 100;
-    let largestNumber = 0;
+    let smallestNumber = banana[0];
+    let largestNumber = banana[1];
     // let secondSmallestNumber = smallestNumber;
     let restOfArray = [];
-    let largestFromRestOfArray = 0;
-    let smallestFromRestOfArray = 100;
+    let largestFromRestOfArray = banana[0];
+    let smallestFromRestOfArray = smallestNumber;
     let answers = [];
 
-    if (banana.length < 2) {
+    if (banana.length <= 2) {
         return 'undefined';
     }
     // finds the smallest number
@@ -29,23 +29,27 @@ function twoSmallest(banana) {
         }
     }
 
-
+// restOfArray = [6,7,5,6,7,5]
     // finds the middle number
     for (let i = 0; i < restOfArray.length; i++) {
         if (restOfArray[i] === smallestNumber || restOfArray[i] === largestNumber) {
             continue;
-
-        } else if (restOfArray[i] > largestFromRestOfArray) {
+        } if (restOfArray[i] > largestFromRestOfArray) {
             largestFromRestOfArray = restOfArray[i];
 
-        } else if(restOfArray[i] < smallestFromRestOfArray) 
+        } else if(restOfArray[i] < smallestFromRestOfArray) {
+            
             smallestFromRestOfArray = restOfArray[i];
+            console.log(smallestFromRestOfArray)
+        }
+            
     }
 
-    answers.push(smallestNumber)
+    answers.push(smallestNumber);
     answers.push(smallestFromRestOfArray);
     return answers;
 
 }
 
-console.log(twoSmallest([10, 2, 6, 7, 5])) // 2,5
+console.log(twoSmallest([122, 277, 409, 99, 5])); // 5,99
+console.log(twoSmallest([7,3])); // undefined
